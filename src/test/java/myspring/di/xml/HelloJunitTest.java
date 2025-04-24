c6013eac6495a27cb54a806cf06abcf88d1d9420
ikh0120 <lkh50635164@gmail.com>
2025-04-24 17:09:48 +0900

    Add JUnit5 Test

--- src/test/java/myspring/di/xml/HelloJunitTest.java
+++ src/test/java/myspring/di/xml/HelloJunitTest.java
@@ -1,10 +1,16 @@
 package myspring.di.xml;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class HelloJunitTest {
+	
 	@Test
 	public void helloBean() {	//public 안써도 됨
-		System.out.println("Hello Junit");
+		//System.out.println("Hello Junit"); // 이렇게 sysout 안쓰고 Assertions 씀
+		//1. Container 객체 생성
+		ApplicationContext context = new GenericXmlApplicationContext("");
 	}
+	
 }
