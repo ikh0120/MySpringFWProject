c64adf35d0b4aa18d5c79c29c716b91fd8218d00
ikh0120 <lkh50635164@gmail.com>
2025-04-24 17:09:33 +0900

    Add JUnit5 Test

---
+++ src/test/java/myspring/di/xml/HelloJunitTest.java
@@ -0,0 +1,10 @@
+package myspring.di.xml;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloJunitTest {
+	@Test
+	public void helloBean() {	//public 안써도 됨
+		System.out.println("Hello Junit");
+	}
+}
