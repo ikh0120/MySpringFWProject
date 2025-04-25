b5dab9fee49a9790e3ea9426d49eacf366f03c58
ikh0120 <lkh50635164@gmail.com>
2025-04-25 10:37:18 +0900

    Add and Use spring-test Repository

--- src/test/java/myspring/di/xml/HelloSpringTest.java
+++ src/test/java/myspring/di/xml/HelloSpringTest.java
@@ -1,10 +1,23 @@
 package myspring.di.xml;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 // SpringExtension 클래스는 ApplicationContext(컨테이너) 객체를 생성하는 역할을 함
 @ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "classpath:hello-di.xml") // 스프링 객체(bean) 설정 파일의 위치를 지정할 때 사용하는 어노테이션
 public class HelloSpringTest {
-
+	@Autowired
+	Hello hello;
+	
+	@Test
+	void helloSpringBean() {
+		
+	}
+	
 }
