7b09870a72ca10e6848eb09588846ef438619d0a
ikh0120 <lkh50635164@gmail.com>
2025-04-28 01:58:57 +0900

    finish study

--- src/main/java/myspring/di/annot/HelloBean.java
+++ src/main/java/myspring/di/annot/HelloBean.java
@@ -12,17 +12,17 @@ import org.springframework.stereotype.Component;
 @Component("helloBean")
 public class HelloBean {
 	
-	@Value("${myName}")
-	String name;
+	@Value("${myName}")//myName="어노테이션"
+	String name;//name="어노테이션"
 	
-//	@Autowired
-//	@Qualifier("stringPrinter")
-	@Resource(name="${myPrinter}")
-	PrinterBean printer;
+/*	@Autowired
+	@Qualifier("stringPrinter")*/
+	@Resource(name="${myPrinter}") //myPrinter='stringPrinter"
+	PrinterBean printer; //printer=stringPrinter
 	
 //	@Value("${names.list.of.strings}")
-	@Value("#{'${names.list.of.strings}'.split(',')}")
-	List<String> names;
+	@Value("#{'${names.list.of.strings}'.split(',')}") 
+	List<String> names; //names=["Java","Spring","Kotlin"]
 
 	public HelloBean() {
 		System.out.println(this.getClass().getName() + " 기본생성자 호출됨!");
@@ -37,26 +37,28 @@ public class HelloBean {
 	public List<String> getNames() {
 		return this.names;
 	}
+/*
+  	public void setNames(List<String> list) {
+		System.out.println("setNames() 메서드 호출됨");
+		this.names = list;
+	}
 
-//	public void setNames(List<String> list) {
-//		System.out.println("setNames() 메서드 호출됨");
-//		this.names = list;
-//	}
-//
-//	public void setName(String name) {
-//		System.out.println("setName() 메서드 호출됨 " + name);
-//		this.name = name;
-//	}
-//
-//	public void setPrinter(PrinterBean printer) {
-//		System.out.println("setPrinter() 메서드 호출됨 " + printer.getClass().getName());
-//		this.printer = printer;
-//	}
+	public void setName(String name) {
+		System.out.println("setName() 메서드 호출됨 " + name);
+		this.name = name;
+	}
+
+	public void setPrinter(PrinterBean printer) {
+		System.out.println("setPrinter() 메서드 호출됨 " + printer.getClass().getName());
+		this.printer = printer;
+	}
+
+ */
 
 	public String sayHello() {
 		return "Hello " + name;
 	}
-
+	
 	public void print() {
 		this.printer.print(sayHello());
 	}
--- src/test/java/myspring/di/annot/HelloBeanSpringTest.java
+++ src/test/java/myspring/di/annot/HelloBeanSpringTest.java
@@ -7,26 +7,37 @@ import javax.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+// ApplicationContext context = new GenericXmlApplicationContext("classpath:hello-di.xml");
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = "classpath:hello-di.xml")
 public class HelloBeanSpringTest {
 	@Autowired
-	HelloBean hello;
+	@Qualifier("helloBean")
+	HelloBean hello; // 우선 HelloBean이라는 id의 빈을 찾음 없으면 HelloBean에서 만든 helloBean이라는 id의 빈을 @Component("helloBean")을 이용해서 만듬
 	
-	@Resource(name = "stringPrinter")
+	@Resource(name="${myPrinter}")
 	PrinterBean printer;
 	
 	@Test
 	void helloBean() {
-		assertEquals("Hello 어노테이션", hello.sayHello());
-		hello.print();		
-		assertEquals("Hello 어노테이션", printer.toString());
+//		assertEquals("Hello 어노테이션", hello.sayHello());
+		hello.print();
+		
+//		assertEquals("Hello 어노테이션", printer.toString());
 		
 		for(String name: hello.getNames()) {
 			System.out.println(name);
 		}
+		System.out.println();
+		System.out.println("hello.name=" + hello.name);
+		System.out.println();
+		System.out.println("Expected: " + "|Hello 어노테이션|");
+		System.out.println("Actual: " + "|"+hello.sayHello()+"|");
+		assertEquals("Hello 어노테이션".trim(), hello.sayHello().trim());
+		
 	}
 }
