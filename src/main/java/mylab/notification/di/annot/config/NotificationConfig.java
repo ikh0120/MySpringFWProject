05e399cafbdc45eff15ec39cdb0e530693fbc378
ikh0120 <lkh50635164@gmail.com>
2025-04-29 21:01:11 +0900

    [1-3]

--- src/main/java/mylab/notification/di/annot/config/NotificationConfig.java
+++ src/main/java/mylab/notification/di/annot/config/NotificationConfig.java
@@ -11,17 +11,18 @@ import mylab.notification.di.annot.SmsNotificationService;
 @Configuration // 스프링이 객체를 생성하고 의존성 주입(DI:Dependency Injection)
 public class NotificationConfig {
 	@Bean
-	public NotificationService emailNotificaitonService() {
-		return new EmailNotificationService("smtp.naver.com", 8765);
+	public EmailNotificationService emailNotificationService() {
+		return new EmailNotificationService("smtp.gmail.com", 587);
 	}
-	
+
 	@Bean
-	public NotificationService smsNotificationService() {
-		return new SmsNotificationService("카카오톡");
+	public SmsNotificationService smsNotificationService() {
+		return new SmsNotificationService("SKT");
 	}
+
 	
 	@Bean
 	public NotificationManager notificationManager() {
-		return new NotificationManager(emailNotificaitonService(), smsNotificationService());
+		return new NotificationManager(emailNotificationService(), smsNotificationService());
 	}
 }
--- src/test/java/mylab/notification/di/annot/config/NotificationConfigTest.java
+++ src/test/java/mylab/notification/di/annot/config/NotificationConfigTest.java
@@ -1,5 +1,93 @@
 package mylab.notification.di.annot.config;
 
-public class NotificationConfigTest {
+import javax.management.Notification;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import mylab.notification.di.annot.EmailNotificationService;
+import mylab.notification.di.annot.NotificationManager;
+import mylab.notification.di.annot.NotificationService;
+import mylab.notification.di.annot.SmsNotificationService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = NotificationConfig.class)
+public class NotificationConfigTest {
+	@Autowired
+	@Qualifier("emailNotificationService")
+	EmailNotificationService ens;
+
+	
+	@Autowired
+	@Qualifier("smsNotificationService")
+	SmsNotificationService sns;
+	
+	@Autowired
+	@Qualifier("notificationManager")
+	NotificationManager nm;
+	
+	@Test
+	void testNotification() {
+		// ens null인지 확인하기
+		assertNotNull(ens);
+		assertNotNull(ens.getPort());
+		assertNotNull(ens.getSmtpServer());
+		
+		// sns null인지 확인하기
+		assertNotNull(sns);
+		assertNotNull(sns.getProvider());
+		
+		// nm null인지 확인하기
+		assertNotNull(nm);
+		assertNotNull(nm.getEmailService());
+		assertNotNull(nm.getSmsService());
+		
+		System.out.println("assertEquals 테스트");
+		System.out.println("인스턴스 별 메서드 테스트");
+		System.out.println("====================");
+		
+		System.out.println("sns.getProvider():" + sns.getProvider());
+		assertEquals("SKT", sns.getProvider());
+		
+		sns.sendNotification("=== SNS Test message ===");
+		
+		System.out.println("--------------------");
+		
+		System.out.println("ens.getPort(): " + ens.getPort());
+		assertEquals(587, ens.getPort());
+		
+		System.out.println("ens.getSmtpServer(): " + ens.getSmtpServer());
+		assertEquals("smtp.gmail.com", ens.getSmtpServer());
+		
+		System.out.println("--------------------");
+		
+		ens.sendNotification("=== ENS Test message ===");
+		
+		System.out.println("--------------------");
+		
+		System.out.println(nm.getEmailService());
+		
+		System.out.println("--------------------");
+		
+		System.out.println(nm.getSmsService());
+		
+		System.out.println("--------------------");
+		
+		nm.getEmailService().sendNotification("hello getEmail sendNotification");
+		nm.getSmsService().sendNotification("hello getSms sendNotification");
+		
+		System.out.println("--------------------");
+		
+		nm.sendNotificationByEmail("hello EMAIL");
+		nm.sendNotificationBySms("hello SMS");
+		
+		
+	}
 }
